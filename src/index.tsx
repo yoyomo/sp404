909a2e1076e204bc10f7e8246963ca43c732caa6
Armando Ortiz <armando@gavel.io>
2024-05-29 23:18:11 -0700

    working react version

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {MantineProvider} from "@mantine/core";
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+root.render(
+    <React.StrictMode>
+        <MantineProvider>
+            <App/>
+        </MantineProvider>
+    </React.StrictMode>
+);
