4818bcf3931f8a5eb05394c0ab55ab1e021d3190
Armando Ortiz <armando.josue.og@gmail.com>
2024-05-30 13:22:35 -0700

    move stuff around

--- src/LoopStart.tsx
+++ src/LoopStart.tsx
@@ -24,20 +24,25 @@ export const LoopStart = () => {
     , [bpm, sampleLength, sampleLengthMode, startPoint, endPoint]);
 
   return (
-    <div className="flex flex-col justify-center items-center text-center gap-y-2 bg-gray-100">
-      <h1 className="text-3xl font-bold m-2"> SP 404 Perfect Loop Start Calculator:</h1>
-      <NumberInput defaultValue={bpm} onUpdate={setBPM} label="BPM:" />
-      <Switch size="lg" onLabel="Sample Length" offLabel="Start/End" color="orange" checked={sampleLengthMode}
+    <div className="flex flex-row bg-gray-100 justify-center h-screen">
+      <div className="flex flex-col items-center text-center gap-y-2">
+        <h1 className="text-3xl font-bold m-2"> SP 404 Perfect Loop Start Calculator:</h1>
+        <NumberInput defaultValue={bpm} onUpdate={setBPM} label="BPM:" />
+
+        {sampleLengthMode ?
+          <NumberInput defaultValue={sampleLength} onUpdate={setSampleLength} label="Sample Length:" />
+          : <div>
+            <NumberInput defaultValue={startPoint} onUpdate={setStartPoint} label="Start:" />
+            <NumberInput defaultValue={endPoint} onUpdate={setEndPoint} label="End:" />
+          </div>
+        }
+
+        <NumberInput defaultValue={numberOfBars} onUpdate={setNumberOfBars} max={maxBars} label={`Number of Bars (max. ${maxBars}):`} />
+        <label>Loop Start:</label>
+        <div className="text-lg">{loopStart}</div>
+      </div>
+      <Switch className="absolute bottom-0 left-0" size="lg" onLabel="Sample Length" offLabel="Start/End" color="orange" checked={sampleLengthMode}
         onChange={(event) => setSampleLengthMode(event.currentTarget.checked)} />
-      {sampleLengthMode ?
-        <NumberInput defaultValue={sampleLength} onUpdate={setSampleLength} label="Sample Length:" />
-        : <div>
-          <NumberInput defaultValue={startPoint} onUpdate={setStartPoint} label="Start:" />
-          <NumberInput defaultValue={endPoint} onUpdate={setEndPoint} label="End:" />
-        </div>}
-      <NumberInput defaultValue={numberOfBars} onUpdate={setNumberOfBars} max={maxBars} label={`Number of Bars (max. ${maxBars}):`} />
-      <label>Loop Start:</label>
-      <div className="text-lg">{loopStart}</div>
     </div>
   );
 }
