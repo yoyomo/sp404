b61e99cec7b11f3a20ecaf762f41524395f838aa
Armando Ortiz <armando.josue.og@gmail.com>
2024-05-30 10:42:40 -0700

    gray background

--- src/LoopStart.tsx
+++ src/LoopStart.tsx
@@ -1,6 +1,6 @@
-import React, {useMemo, useState} from 'react';
-import {getLoopStart} from "./utils";
-import {NumberInput} from "./NumberInput";
+import { useMemo, useState } from 'react';
+import { getLoopStart } from "./utils";
+import { NumberInput } from "./NumberInput";
 
 export const LoopStart = () => {
   const [bpm, setBPM] = useState(90);
@@ -8,15 +8,15 @@ export const LoopStart = () => {
   const [numberOfBars, setNumberOfBars] = useState(4);
 
   const loopStart = useMemo(() =>
-      getLoopStart(bpm, sampleLength, numberOfBars)
+    getLoopStart(bpm, sampleLength, numberOfBars)
     , [bpm, sampleLength, numberOfBars]);
 
   return (
-    <div className="flex flex-col justify-center items-center text-center gap-y-2">
+    <div className="flex flex-col justify-center items-center text-center gap-y-2 bg-gray-100">
       <h1 className="text-3xl font-bold m-2"> SP 404 Perfect Loop Start Calculator:</h1>
-      BPM: <NumberInput value={bpm} onUpdate={setBPM}/>
-      Sample Length: <NumberInput value={sampleLength} onUpdate={setSampleLength}/>
-      Number of Bars: <NumberInput value={numberOfBars} onUpdate={setNumberOfBars}/>
+      BPM: <NumberInput value={bpm} onUpdate={setBPM} />
+      Sample Length: <NumberInput value={sampleLength} onUpdate={setSampleLength} />
+      Number of Bars: <NumberInput value={numberOfBars} onUpdate={setNumberOfBars} />
       <label>Loop Start:</label>
       <div className="text-lg">{loopStart}</div>
     </div>
