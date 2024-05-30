5da0eddcb5dd71b3f7d658a8e1999e404c252691
Armando Ortiz <armando.josue.og@gmail.com>
2024-05-30 00:59:54 -0700

    larger loop start text

--- src/LoopStart.tsx
+++ src/LoopStart.tsx
@@ -9,17 +9,17 @@ export const LoopStart = () => {
 
   const loopStart = useMemo(() =>
       getLoopStart(bpm, sampleLength, numberOfBars)
-  , [bpm, sampleLength, numberOfBars]);
+    , [bpm, sampleLength, numberOfBars]);
 
 
   return (
     <div className="flex flex-col justify-center items-center text-center gap-y-2">
-        <h1 className="text-3xl font-bold m-2"> SP 404 Perfect Loop Start Calculator:</h1>
-      BPM: <NumberInput value={bpm} onUpdate={setBPM} />
-      Sample Length: <NumberInput value={sampleLength} onUpdate={setSampleLength} />
-      Number of Bars: <NumberInput value={numberOfBars} onUpdate={setNumberOfBars} />
-
-      <div>Loop Start: {loopStart}</div>
+      <h1 className="text-3xl font-bold m-2"> SP 404 Perfect Loop Start Calculator:</h1>
+      BPM: <NumberInput value={bpm} onUpdate={setBPM}/>
+      Sample Length: <NumberInput value={sampleLength} onUpdate={setSampleLength}/>
+      Number of Bars: <NumberInput value={numberOfBars} onUpdate={setNumberOfBars}/>
+      <label>Loop Start:</label>
+      <div className="text-lg">{loopStart}</div>
     </div>
   );
 }
